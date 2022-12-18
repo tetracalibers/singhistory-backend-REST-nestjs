9a16bc52bfe4f64d7b5c1ac49f4422e92aed1c2a
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-12-18 17:51:05 +0900

    UserEntityをtypeOrmに登録

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 import { SingHistory } from './sing-history/entities/sing-history.entity';
 import { SingHistoryModule } from './sing-history/sing-history.module';
 import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -23,7 +24,7 @@ import { UsersModule } from './users/users.module';
         username: config.get('POSTGRES_USER'),
         password: config.get('POSTGRES_PASSWORD'),
         database: config.get('POSTGRES_DB'),
-        entities: [SingHistory],
+        entities: [SingHistory, User],
         synchronize: config.get('NODE_ENV') !== 'production',
       }),
     }),
