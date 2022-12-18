d56162eef47e53d78c7f92955cd9f2d1f22f7117
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-12-18 13:45:08 +0900

    SingHistoryEntityの定義

--- src/sing-history/entities/sing-history.entity.ts
+++ src/sing-history/entities/sing-history.entity.ts
@@ -1,1 +1,46 @@
-export class SingHistory {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class SingHistory {
+  @PrimaryGeneratedColumn('increment', { type: 'int' })
+  readonly id: number;
+
+  @Column()
+  artistId: string;
+
+  @Column()
+  artistName: string;
+
+  @Column()
+  songName: string;
+
+  @Column({ nullable: true })
+  jacketUrl?: string;
+
+  @Column()
+  singDate: string;
+
+  @Column({ type: 'int', default: 0 })
+  singKey: number;
+
+  @Column({ type: 'int', default: 0, unsigned: true })
+  rating: number;
+
+  @Column({ type: 'int', unsigned: true, nullable: true })
+  score?: number;
+
+  @Column({ nullable: true })
+  memo?: string;
+
+  @CreateDateColumn()
+  readonly createdAt: Date;
+
+  @UpdateDateColumn()
+  readonly updatedAt: Date;
+}
